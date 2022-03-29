2b4f282f4521e084ca54992b2dc0553185274599
dilan-lasith <59189547+dilan-lasith@users.noreply.github.com>
2022-03-30 01:17:02 +0530

    dilan

--- slbfe_citizenapp/lib/main.dart
+++ slbfe_citizenapp/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:slbfe_citizenapp/view/accountSetting.dart';
 import 'package:slbfe_citizenapp/view/login.dart';
+import 'package:slbfe_citizenapp/view/resetPassword.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Login(),
+      home: accountSetting(),
     );
   }
 }
