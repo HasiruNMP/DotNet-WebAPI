be77621f0167383bade1d3189bab878e565d9e9b
Ravindu Arsakulasooriya <56332270+Ravindu78@users.noreply.github.com>
2022-03-30 01:45:52 +0530

    profile

--- slbfe_citizenapp/lib/view/profile.dart
+++ slbfe_citizenapp/lib/view/profile.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:slbfe_citizenapp/view/contacts.dart';
-import 'package:slbfe_citizenapp/view/personaldetails.dart';
-
-import 'documents.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
@@ -15,14 +11,10 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-        backgroundColor: Colors.blue,
-      ),
+      appBar: AppBar(title: const Text('Profile'),backgroundColor: Colors.blue,),
       body: SafeArea(
         child: Column(
           children: [
-
            Center(
              child: InkWell(
                child: ImageIcon( AssetImage("assets/profile.png"),
@@ -86,55 +78,14 @@ class _ProfileState extends State<Profile> {
                       ],
                     ),
                   ],
-
-            Center(
-              child: InkWell(
-                child: ImageIcon(
-                  AssetImage("assets/profile.png"),
-                  color: Colors.black26,
-                  size: 110,
-
                 ),
-              ),
+              )
             ),
-            Container(
-                height: 200,
-                width: 360,
-                color: Colors.black26,
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [Icon(Icons.person), Text("name")],
-                      ),
-                      SizedBox(
-                        height: 13,
-                      ),
-                      Row(
-                        children: [Icon(Icons.mail), Text("E-Mail")],
-                      ),
-                      SizedBox(
-                        height: 13,
-                      ),
-                      Row(
-                        children: [Icon(Icons.phone), Text("Phone")],
-                      ),
-                      SizedBox(
-                        height: 13,
-                      ),
-                      Row(
-                        children: [Icon(Icons.perm_identity), Text("NIC")],
-                      ),
-                    ],
-                  ),
-                )),
             SizedBox(
               height: 15,
             ),
             SizedBox(
               width: 360,
-
               child: ElevatedButton(onPressed: (){}, child: const Text('More Details'),style: ButtonStyle(
                   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                       RoundedRectangleBorder(
@@ -143,28 +94,12 @@ class _ProfileState extends State<Profile> {
                       )
                   )
               )),
-
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => PersonaInfo()),
-                    );
-                  },
-                  child: const Text('More Details'),
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18.0),
-                              side: BorderSide(color: Colors.red))))),
-
             ),
             SizedBox(
               height: 9,
             ),
             SizedBox(
               width: 360,
-
               child: ElevatedButton(onPressed: (){}, child: const Text('Documents'),style: ButtonStyle(
                   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                       RoundedRectangleBorder(
@@ -173,51 +108,26 @@ class _ProfileState extends State<Profile> {
                       )
                   )
               )),
-
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Documents()),
-                    );
-                  },
-                  child: const Text('Documents'),
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18.0),
-                              side: BorderSide(color: Colors.red))))),
-
             ),
             SizedBox(
               height: 9,
             ),
             SizedBox(
               width: 360,
-              child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Location'),
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              child: ElevatedButton(onPressed: (){}, child: const Text('Location'),style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                       RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(18.0),
-
                           side: BorderSide(color: Colors.blue)
                       ),
                   ),
               )),
-
-                          side: BorderSide(color: Colors.red)),
-                    ),
-                  )),
-
             ),
             SizedBox(
               height: 9,
             ),
             SizedBox(
               width: 360,
-
               child: ElevatedButton(onPressed: (){}, child: const Text('Contacts'),style: ButtonStyle(
                   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                       RoundedRectangleBorder(
@@ -226,28 +136,12 @@ class _ProfileState extends State<Profile> {
                       )
                   )
               )),
-
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Contacts()),
-                    );
-                  },
-                  child: const Text('Contacts'),
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18.0),
-                              side: BorderSide(color: Colors.red))))),
-
             ),
             SizedBox(
               height: 9,
             ),
             SizedBox(
               width: 360,
-
               child: ElevatedButton(onPressed: (){}, child: const Text('Account'),style: ButtonStyle(
                   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                       RoundedRectangleBorder(
@@ -256,19 +150,13 @@ class _ProfileState extends State<Profile> {
                       )
                   )
               )),
-             child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Account'),
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18.0),
-                              side: BorderSide(color: Colors.red))))),
-
             ),
           ],
+          
         ),
       ),
+      
     );
   }
 }
+
