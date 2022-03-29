37daeed4649db3fda943f9731819e9b56fbb7dc2
Ravindu Arsakulasooriya <56332270+Ravindu78@users.noreply.github.com>
2022-03-30 00:51:48 +0530

    profile

--- slbfe_citizenapp/lib/view/profile.dart
+++ slbfe_citizenapp/lib/view/profile.dart
@@ -10,10 +10,106 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        'profile',
+    return Scaffold(
+      appBar: AppBar(title: const Text('Profile'),backgroundColor: Colors.blue,),
+      body: SafeArea(
+        child: Column(
+          children: [
+           Center(
+             child: InkWell(
+               child: ImageIcon( AssetImage("assets/profile.png"),
+                    color: Colors.black26,
+                    size: 110,),
+             ),
+
+
+             
+           ),
+
+            Container(
+              height: 200,
+              width: 360,
+              color: Colors.black26,
+              child:Padding(
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: Column(
+                  children: [
+
+                    Row(
+                        children: [
+                          Icon(Icons.person),
+                          Text("name")
+                        ],
+                      ),
+
+                    Row(
+                      children: [
+                        Icon(Icons.mail),
+                        Text("E-Mail")
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.phone),
+                        Text("Phone")
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.perm_identity),
+                        Text("NIC")
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ),
+            ElevatedButton(onPressed: (){}, child: const Text('More Details'),style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.red)
+                    )
+                )
+            )),
+            ElevatedButton(onPressed: (){}, child: const Text('Documents'),style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.red)
+                    )
+                )
+            )),
+            ElevatedButton(onPressed: (){}, child: const Text('Location'),style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.red)
+                    )
+                )
+            )),
+            ElevatedButton(onPressed: (){}, child: const Text('Contacts'),style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.red)
+                    )
+                )
+            )),
+            ElevatedButton(onPressed: (){}, child: const Text('Account'),style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.red)
+                    )
+                )
+            )),
+          ],
+          
+        ),
       ),
+      
     );
   }
 }
+
