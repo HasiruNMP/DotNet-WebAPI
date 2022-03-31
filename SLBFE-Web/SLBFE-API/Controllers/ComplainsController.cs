6034fe77a4e52ea069bde970b13fcf19ee15f651
Dinith Tennakoon <53987995+iamdinith@users.noreply.github.com>
2022-04-01 00:34:41 +0530

    dinith's commit - 1

---
+++ SLBFE-Web/SLBFE-API/Controllers/ComplainsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using SLBFE_API.Models;
+
+namespace SLBFE_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComplainsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ComplainsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult GetComplain(int NIC)
+        {
+            string query = @"SELECT * FROM [dbo].[JS_COMPLAINS] WHERE JS_NIC =" + NIC;
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SLBFEDB");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPut]
+        public JsonResult PutFeedback(JsComplain comp)
+        {
+            string query = @"UPDATE [dbo].[JS_COMPLAINS] SET Feedback = @Feedback WHERE ComplaintID = @ComplaintID";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SLBFEDB");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@ComplaintID", comp.ComplaintId);
+                    myCommand.Parameters.AddWithValue("@Feedback", comp.Feedback);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Updated Successfully");
+        }
+
+    }
+
+    
+}
