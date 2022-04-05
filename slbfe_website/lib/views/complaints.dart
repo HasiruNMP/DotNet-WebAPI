cee5a0f59deee7da7489202244e11f817c70e7bd
HasiruN <hasirunmp@gmail.com>
2022-04-06 00:24:09 +0530

    complaint ui

--- slbfe_website/lib/views/complaints.dart
+++ slbfe_website/lib/views/complaints.dart
@@ -1,3 +1,4 @@
+import 'package:buttons_tabbar/buttons_tabbar.dart';
 import 'package:flutter/material.dart';
 
 class Complaints extends StatefulWidget {
@@ -8,27 +9,150 @@ class Complaints extends StatefulWidget {
 }
 
 class _ComplaintsState extends State<Complaints> {
-  
-  //final _controller = TabController(length: 2, vsync: null);
-  
+  //late TabController _controller;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       //color: Colors.blueGrey.shade50,
-      height: MediaQuery.of(context).size.height/1,
-      child: DefaultTabController(
-        length: 2,
-        child: TabBar(
-          tabs: [
-            new Tab(
-              icon: const Icon(Icons.home),
-              text: 'Address',
+      //height: MediaQuery.of(context).size.height/1,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                //color: Colors.deepPurple,
+                child: DefaultTabController(
+                  length: 2,
+                  child: Column(
+                    children: <Widget>[
+                      ButtonsTabBar(
+                        backgroundColor: Colors.indigo,
+                        unselectedBackgroundColor: Colors.grey[300],
+                        unselectedLabelStyle: TextStyle(color: Colors.black),
+                        labelStyle: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                        tabs: [
+                          Tab(
+                            //icon: Icon(Icons.directions_car),
+                            text: "   New Complaints   ",
+                          ),
+                          Tab(
+                            //icon: Icon(Icons.directions_transit),
+                            text: "   Complaint History   ",
+                          ),
+                        ],
+                      ),
+                      Divider(),
+                      Expanded(
+                        child: TabBarView(
+                          children: <Widget>[
+                            ListView(
+                              children: [
+                                ComplaintCard(),
+                              ],
+                            ),
+                            ListView(
+                              children: [
+                                ComplaintCard(),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-            new Tab(
-              icon: const Icon(Icons.my_location),
-              text: 'Location',
+            VerticalDivider(),
+            Expanded(
+              flex: 5,
+              child: Container(
+                //color: Colors.green,
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text("NIC"),
+                      subtitle: Text("date"),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          enabled: false,
+                          minLines: 20,
+                          maxLines: 20,
+                          keyboardType: TextInputType.multiline,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Complaint',
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          minLines: 20,
+                          maxLines: 20,
+                          keyboardType: TextInputType.multiline,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Response',
+                          ),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Expanded(child: Container(),),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text("SEND"),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(child: Container(),),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class ComplaintCard extends StatelessWidget {
+  const ComplaintCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 1,horizontal: 8),
+      child: Card(
+        color: Colors.teal.shade100,
+        child: InkWell(
+          onTap: (){},
+          child: ListTile(
+            title: Text("NIC, Date Time"),
+            subtitle: Text("preview"),
+          ),
         ),
       ),
     );
