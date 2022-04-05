b6b6c30e92f70b80631234ff894935073a20eeee
Ravindu Arsakulasooriya <56332270+Ravindu78@users.noreply.github.com>
2022-04-05 23:12:40 +0530

    sign in

---
+++ slbfe_website/lib/views/signin.dart
@@ -0,0 +1,157 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+// import 'package:slbfe_citizenapp/api/apiservice.dart';
+// import 'package:slbfe_citizenapp/model/loginmodel.dart';
+// import 'package:slbfe_citizenapp/view/registration.dart';
+// import 'package:slbfe_citizenapp/utilities//global.dart' as global;
+// import 'bottomnavigation.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _userEmailController = TextEditingController();
+  final TextEditingController _userPassworController = TextEditingController();
+  // LoginRequestModel requestModel = LoginRequestModel(email: '', password: '');
+  bool loading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text('Sign In'),
+          ),
+        ),
+        body: SafeArea(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 100,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: _userEmailController,
+                    decoration: const InputDecoration(
+                      hintText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email address';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: _userPassworController,
+                    decoration: const InputDecoration(
+                      hintText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 5) {
+                        return 'Please enter a long password';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                 ElevatedButton(
+                  onPressed: () async {
+                    //   if (_formKey.currentState!.validate()) {
+                    //     requestModel.email = _userEmailController.text;
+                    //     requestModel.password = _userPassworController.text;
+                    //     //  print(requestModel.toJson());
+                    //
+                    //     int login = await APIService.login(requestModel);
+                    //     //  print("status: $login");
+                    //     if (login == 0) {
+                    //       showAlertDialog(
+                    //           'Wrong password or email provided', context);
+                    //     } else if (login == -1) {
+                    //       showAlertDialog('Something went Wrong', context);
+                    //     } else {
+                    //       Navigator.push(
+                    //         context,
+                    //         MaterialPageRoute(
+                    //           builder: (context) => BottomNavigation(login),
+                    //         ),
+                  }, child: null,
+                        )
+                  //     }
+                  //   } else {
+                  //     return null;
+                  //   }
+                  // },
+                  // child: loading == false
+                  //     ? Text('Sign In')
+                  //     : const CircularProgressIndicator(
+                  //         backgroundColor: Colors.black38,
+                  //         valueColor:
+                  //             AlwaysStoppedAnimation<Color>(Colors.white),
+                  //       ),
+                // ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   children: [
+                //     Text('Don\t have an account'),
+                //     TextButton(
+                //       onPressed: () {
+                //         Navigator.push(
+                //           context,
+                //           MaterialPageRoute(
+                //             builder: (context) => Registration(),
+                //           ),
+                //         );
+                //       },
+                //       child: Text('Sign In'),
+                //     ),
+                //   ],
+                // ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+showAlertDialog(message, BuildContext context) {
+  // set up the button
+  Widget okButton = FlatButton(
+    child: Text("OK"),
+    onPressed: () {
+      Navigator.pop(context);
+    },
+  );
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("Alert Box"),
+    content: Text(message),
+    actions: [
+      okButton,
+    ],
+  );
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
